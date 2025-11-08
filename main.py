dce6cff1807a00d041d483afbddd1fcdccfa68b5
Lakshya-2408 <104901308+Lakshya-2408@users.noreply.github.com>
2025-11-09 00:15:19 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from gui import SmartTaskManagerGUI
-
-if __name__ == "__main__":
-    print("Starting Smart Task Manager...")
-    print("Team: Avengers")
-    print("Project: Intelligent Process Categorisation and Automated Task Termination")
-    
-    app = SmartTaskManagerGUI()
-    app.run()
